2c13d5651e6efc2f113313b3b74ae611080bd87e
Pascal Nague <pana01@dfki.de>
2026-09-11 10:11:10 +0200

    Prepare package for PyPI publishing

--- minimal_dataset/__init__.py
+++ minimal_dataset/__init__.py
@@ -1,19 +1,27 @@
 """
 Minimal Dependency Dataset Library for PyTorch.
 
-Usage:
-    from minimal_dataset import BaseDataset, DataLoader
-
-    dataset = BaseDataset("/path/to/data")
-    loader = DataLoader(dataset, batch_size=32, num_workers=4)
-    for images, labels in loader:
-        # train...
+Experimental data-loading library developed as part of a Bachelor Project
+at DFKI.
 """
+
 from .dataset import BaseDataset
+from .parquet_dataset import ParquetDataset
+from .dataloader import DataLoader
 from .dataloader_mp import DataLoaderMP
 from .sampler import LockFreeSampler
-from .dataloader import DataLoader
 from .monitored_queue import MonitoredQueue
 from .metrics import MetricsTracker, WorkerMetrics
-from .parquet_dataset import ParquetDataset
-from .dataloader_mp import DataLoaderMP
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "BaseDataset",
+    "ParquetDataset",
+    "DataLoader",
+    "DataLoaderMP",
+    "LockFreeSampler",
+    "MonitoredQueue",
+    "MetricsTracker",
+    "WorkerMetrics",
+]
